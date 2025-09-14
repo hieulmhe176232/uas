9e3f1da1f7e1a8d06ffc0d9b5a9a7d7a6005cc6b
hieulmhe176232 <hieulmhe176232@fpt.edu.vn>
2025-09-14 14:35:41 +0700

    setup spring (#2)
    Co-authored-by: twikou <eu.hi3107@gmail.com>

---
+++ backend/src/test/java/capstone/fpt/uas/UasApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.fpt.uas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
